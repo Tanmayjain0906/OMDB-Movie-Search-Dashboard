e358b972169b5932112d99b3c081c16f9015f16a
Tanmayjain0906 <tanmayjain983@gmail.com>
2023-09-16 13:26:52 +0530

    movie app

---
+++ script.js
@@ -0,0 +1,56 @@
+
+let baseUrl = "https://www.omdbapi.com/";
+let apiKey;
+
+
+const form = document.getElementsByTagName("form")[0];
+const container = document.getElementsByClassName("card-container")[0];
+
+async function fetchData(search) {
+    let url = `${baseUrl}?s=${search}&apikey=${apiKey}`
+
+    let response = await fetch(url);
+    let data = await response.json();
+
+    if (data.Response == "False") {
+        alert(data.Error);
+    }
+    else {
+        addDataToUI(data.Search);
+    }
+}
+
+form.addEventListener("submit", (e) => {
+
+    e.preventDefault();
+    const apiSearchBar = document.getElementById("apiKey").value;
+    const searchBar = document.getElementById("search").value;
+
+    apiKey = apiSearchBar;
+
+    fetchData(searchBar);
+})
+
+function addDataToUI(data)
+{
+    console.log(data);
+    let count = 1;
+    data.forEach((element) => {
+        let card = document.createElement("div");
+        card.className = "card";
+
+        card.innerHTML = `
+        <div class="poster-img">
+        <img src="${element.Poster}" alt="Something Went Wrong">
+        </div>
+
+        <div class="lower">
+            <h1 id="item-no">${count}</h1>
+            <h2 class="movie-name">${element.Title}(${element.Year})</h2>
+        </div>`
+
+        count++;
+
+        container.appendChild(card);
+    })
+}
